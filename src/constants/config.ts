e199661f2745d4e6a9de4c7afcceed43b057f597
green <masllonishe@gmail.com>
2024-04-07 19:51:23 +0200

    deploy

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,12 +40,12 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
+    title: "Maslineze",
+    fullName: "Alex",
     email: "johndoe@mail.com",
   },
   hero: {
-    name: "John Doe",
+    name: "Alex",
     p: ["I develop 3D visuals, user", "interfaces and web applications"],
   },
   contact: {
